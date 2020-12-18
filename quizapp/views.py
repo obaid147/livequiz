b49d7e31629b922c65d24fbc653e335150fc6b5f
Obaid <you@example.com>
2020-12-18 15:51:33 +0530

    addeed start page and dynamic title and headings

--- quizapp/views.py
+++ quizapp/views.py
@@ -10,4 +10,17 @@ def index(request):
 	paginator = Paginator(questions, 1)  # Show 25 contacts per page.
 	page_number = request.GET.get('page')
 	page_obj = paginator.get_page(page_number)
-	return render(request, 'index.html', {'page_obj': page_obj})
+	context = {
+		'heading': 'Quiz',
+		'title': 'Quiz-App',
+		'page_obj': page_obj,
+	}
+	return render(request, 'index.html', context)
+
+
+def start(request):
+	context = {
+		'heading': 'Home',
+		'title': 'Home Page',
+	}
+	return render(request, 'start.html', context)
