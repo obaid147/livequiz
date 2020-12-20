77d2ae5f3da97ac3b0cabcd18c4737214ed4aece
Obaid <you@example.com>
2020-12-20 11:05:45 +0530

    comment

--- quizapp/views.py
+++ quizapp/views.py
@@ -6,7 +6,7 @@ from .models import Quiz
 def index(request):
 	questions = Quiz.objects.all()
 
-	paginator = Paginator(questions, 1)  # Show 25 contacts per page.
+	paginator = Paginator(questions, 1)  # Show 1 contacts per page.
 	page_number = request.GET.get('page')
 	page_obj = paginator.get_page(page_number)
 	context = {
