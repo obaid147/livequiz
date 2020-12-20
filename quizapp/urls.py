54ff332097ed896b9d9e6764ac534f74b5cd8aaa
Obaid <you@example.com>
2020-12-20 12:58:30 +0530

    login template added

--- quizapp/urls.py
+++ quizapp/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
 	path('', views.start, name='start'),
-	path('index', views.index, name='index'),
-	path('login', views.login, name='login'),
+	path('index/', views.index, name='index'),
+	path('login/', views.login, name='login'),
 
 ]
--- quizapp/views.py
+++ quizapp/views.py
@@ -1,35 +1,49 @@
+from django.contrib import messages
+from django.contrib.auth.models import User, auth
 from django.core.paginator import Paginator
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Quiz
 
 
 def index(request):
-	questions = Quiz.objects.all()
+    questions = Quiz.objects.all()
 
-	paginator = Paginator(questions, 1)  # Show 1 contacts per page.
-	page_number = request.GET.get('page')
-	page_obj = paginator.get_page(page_number)
-	context = {
-		'heading': 'Quiz',
-		'title': 'Quiz-App',
-		'page_obj': page_obj,
-	}
-	return render(request, 'index.html', context)
+    paginator = Paginator(questions, 1)  # Show 1 contacts per page.
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'heading': 'Quiz',
+        'title': 'Quiz-App',
+        'page_obj': page_obj,
+    }
+    return render(request, 'index.html', context)
 
 
 def start(request):
-	context = {
-		'heading': 'Home',
-		'title': 'Home Page',
-	}
+    context = {
+        'heading': 'Home',
+        'title': 'Home Page',
+    }
 
-	return render(request, 'start.html', context)
+    return render(request, 'start.html', context)
 
 
 def login(request):
-	context = {
-		'heading': 'Home',
-		'title': 'Home Page',
-	}
-
-	return render(request, 'login.html', context)
+    users = User.objects.all()
+    contents = {
+        'title': 'Login',
+        'heading': 'Login',
+        'users': users
+    }
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('download')
+        else:
+            messages.info(request, 'Invalid\nUsername or Password')
+            return redirect('login')
+    else:
+        return render(request, 'login.html', contents)
