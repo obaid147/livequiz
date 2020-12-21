00a37ac88cb82ff8fab2c4869b29d43ebfda8ee5
obaid147 <obaidfayazwani@gmail.com>
2020-12-21 13:56:01 +0530

    added scoring

--- quizapp/models.py
+++ quizapp/models.py
@@ -6,8 +6,8 @@ from django.db import models
 class Quiz(models.Model):
     question_number = models.IntegerField(default=True)
     question = models.TextField(max_length=500, default=True)
-    option1 = models.CharField(max_length=300)
-    option2 = models.CharField(max_length=300)
-    option3 = models.CharField(max_length=300)
-    option4 = models.CharField(max_length=300)
-    answer = models.CharField(max_length=300)
+    option1 = models.CharField(max_length=300, default=True)
+    option2 = models.CharField(max_length=300, default=True)
+    option3 = models.CharField(max_length=300, default=True)
+    option4 = models.CharField(max_length=300, default=True)
+    answer = models.CharField(max_length=300, default=True)
