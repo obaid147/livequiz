59dfb973456830cbe99bf9fb96fd89f31c1e688c
Obaid <you@example.com>
2021-01-02 00:53:58 +0530

    Password Reset added with full functionality

--- quiz/settings.py
+++ quiz/settings.py
@@ -156,3 +156,11 @@ AUTHENTICATION_BACKENDS = {
 SITE_ID = 1
 
 LOGIN_REDIRECT_URL = '/'
+
+# for localhost , for wadeff turn-on less secure app access here https://myaccount.google.com/u/1/security?pli=1
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'wadefff@gmail.com'
+EMAIL_HOST_PASSWORD = 'wadefffobaid'
--- quizapp/urls.py
+++ quizapp/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -7,4 +9,15 @@ urlpatterns = [
     path('signup', views.signup, name='signup'),
     path('logout/', views.logout_req, name='logout'),
     path('login/', views.login_req, name='login'),
+
+    # reset password urls
+    path('reset_password/', auth_views.PasswordResetView.as_view(
+        template_name='regestration/password_reset_form.html'), name='reset_password'),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(
+        template_name='regestration/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(
+        template_name='regestration/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset_password_complete', auth_views.PasswordResetCompleteView.as_view(
+        template_name='regestration/password_reset_complete.html'), name='password_reset_complete'),
+
 ]
