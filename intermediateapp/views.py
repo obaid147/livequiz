bc7b80fe9390bd7e15635bfdd148edbd978e7066
Obaid <you@example.com>
2021-01-03 21:23:16 +0530

    Heading Changed

--- intermediateapp/views.py
+++ intermediateapp/views.py
@@ -12,7 +12,7 @@ def inter_view(request):
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     context = {
-        'heading': 'Intermediate',
+        'heading': 'Intermediate Quiz',
         'title': 'Quiz-App',
         'page_obj': page_obj,
     }
