a5ccb0b376dd2848a7d7839a430fc7ea89c81e92
Obaid <you@example.com>
2021-01-03 16:30:15 +0530

    Subdirectory of url changed to uuid

--- intermediateapp/urls.py
+++ intermediateapp/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from . import views
+import uuid
+
+
+uuid_one = str(uuid.uuid4())
+uuid_two = str(uuid.uuid4())
+uuid_intermediate = 'i_n_' + uuid_one + uuid_two + '_t_r'
 
 urlpatterns = [
-    path('intermediate', views.inter_view, name='intermediate'),
+    path(f'{uuid_intermediate}', views.inter_view, name='intermediate'),
 ]
