e098c57efbe7d4c97e6d9274cdd79e23de34f0a1
Obaid <you@example.com>
2021-01-03 16:32:07 +0530

    Subdirectory of url changed to uuid

--- advancedapp/urls.py
+++ advancedapp/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from . import views
+import uuid
+
+
+uuid_one = str(uuid.uuid4())
+uuid_two = str(uuid.uuid4())
+uuid_advanced = 'a_d_' + uuid_one + uuid_two + '_v_c'
 
 urlpatterns = [
-    path('advanced', views.adv_view, name='advanced'),
+    path('{}'.format(uuid_advanced), views.adv_view, name='advanced'),
 ]
